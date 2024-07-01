4174b06ee9ff90bd02eba5eb49afe2b9a9d1cf92
ilasingh20 <ns.ilasingh@gmail.com>
2024-07-01 13:19:16 +0530

    codeRefactor

---
+++ src/main/java/com/example/yatra_ms/Bus.java
@@ -0,0 +1,10 @@
+package com.example.yatra_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Bus {
+    @GetMapping("/Bus")
+    public String getData() {return  "Please book your flight ticket from yatra.com on  50% discount" ; }
+}
